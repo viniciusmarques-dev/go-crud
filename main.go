2086c2d4ac1727ca5609d804f0b9b62f6dd9a1d7
Vinicius <viniciusmarques.dev@gmail.com>
2025-05-13 17:07:00 -0300

    feat: get, post & delete

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Contact struct {
@@ -18,11 +18,95 @@ type ContactService struct {
 	Contacts map[int]Contact
 }
 
+func (c *ContactService) Create(w http.ResponseWriter, r *http.Request) {
+	var contact Contact
+	err := json.NewDecoder(r.Body).Decode(&contact)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := len(c.Contacts) + 1
+	contact.Id = id
+
+	c.Contacts[id] = contact
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(contact)
+}
+
+func (c *ContactService) Get(w http.ResponseWriter, r *http.Request, id int) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if val, ok := c.Contacts[id]; ok {
+		json.NewEncoder(w).Encode(val)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func (c *ContactService) List(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var contacts []Contact
+
+	for _, ct := range c.Contacts {
+		contacts = append(contacts, ct)
+	}
+
+	json.NewEncoder(w).Encode(contacts)
+}
+
+func (c *ContactService) Delete(w http.ResponseWriter, r *http.Request, id int) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, ok := c.Contacts[id]; ok {
+		delete(c.Contacts, id)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func handleGetContacts(w http.ResponseWriter, r *http.Request, service *ContactService) {
+	query := r.URL.Query()
+	if query.Get("id") != "" {
+		id, _ := strconv.Atoi(query.Get("id"))
+		service.Get(w, r, id)
+	} else {
+		service.List(w, r)
+	}
+}
+
+func handleDeleteContact(w http.ResponseWriter, r *http.Request, service *ContactService) {
+	q := r.URL.Query()
+	if q.Get("id") != "" {
+		id, _ := strconv.Atoi(q.Get("id"))
+		service.Delete(w, r, id)
+	} else {
+		http.Error(w, "Contact not found", http.StatusNotFound)
+	}
+}
+
+func handleCreateContact(w http.ResponseWriter, r *http.Request, service *ContactService) {
+	service.Create(w, r)
+}
+
 func main() {
+	service := &ContactService{Contacts: make(map[int]Contact)}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello", html.EscapeString(r.URL.Path))
+	mux.HandleFunc("/contacts", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handleGetContacts(w, r, service)
+		case http.MethodPost:
+			handleCreateContact(w, r, service)
+		case http.MethodDelete:
+			handleDeleteContact(w, r, service)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":4000", mux))
 }
